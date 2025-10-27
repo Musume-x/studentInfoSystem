7f151ffb2f16b402aa688127626631879e80136e
Musume-x <earlgimenez@gmail.com>
2025-10-27 20:14:21 +0800

    system added

--- src/config/config.java
+++ src/config/config.java
@@ -9,7 +9,7 @@ public class config {
         Connection con = null;
         try {
             Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
-            con = DriverManager.getConnection("jdbc:sqlite:student_infoSystem.db"); // Establish connection
+            con = DriverManager.getConnection("jdbc:sqlite:student_infoSys.db"); // Establish connection
             //System.out.println("Connection Successful");
         } catch (Exception e) {
             System.out.println("Connection Failed: " + e);
